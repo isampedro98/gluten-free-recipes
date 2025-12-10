b54f547157cd3379593fe48d051270567461ea7e
isampedro98 <isampedro98@gmail.com>
2025-12-10 19:08:10 -0300

    fix: match nuxt config key order for lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: {
+    enabled: true
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui'
   ],
-
-  devtools: {
-    enabled: true
-  },
 
   nitro: {
     preset: 'github-pages'
